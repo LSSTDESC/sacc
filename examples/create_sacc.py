4692978182984fcd759402195e283b8848234497
Tom McClintock <tmcclintock89@gmail.com>
2018-09-19 11:23:55 -0700

    Revered the create_sacc.py example to match what is on Master.

--- examples/create_sacc.py
+++ examples/create_sacc.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
-import os, sys
-import numpy as np
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 import sacc
+import numpy as np
 
 ## We will write the following toy example:.
 ##
@@ -45,95 +43,33 @@ for i,z in enumerate([0.5,0.7,0.9,1.1]):
 # and also add CMB
 tracers.append (sacc.Tracer("Planck","spin0", None, None))
 
-## Clusters with 5 richness bins and 4 tomographic bins:
-lambda_bins = [20,30,50,80,120,180]
-for i,z in enumerate([0.3,0.5,0.7,0.9]):
-    zar=np.arange(z-0.1,z+0.1,0.001)
-    Nz=np.exp(-(z-zar)**2/(2*0.03**2))
-    for j in range(0,len(lambda_bins)-1):
-        l_min = lambda_bins[j]
-        l_max = lambda_bins[j+1]
-
-        T=sacc.Tracer("clusters_z%i_l%i"%(i,j),"spin0",zar,Nz,exp_sample="clusters", Mproxy_name = "richness", Mproxy_min = l_min, Mproxy_max = l_max)
-        tracers.append(T)
-
-## source galaxies with 4 redshift bins
-for i,z in enumerate([0.5,0.7,0.9,1.1]):
-    zar=np.arange(z-0.1,z+0.1,0.001)
-    Nz=np.exp(-(z-zar)**2/(2*0.025**2))
-    DNz=np.zeros((len(Nz),2))
-    ## some random shapes of Nz to marginalise over
-    DNz[:,0]=(z-zar)**2*0.01
-    DNz[:,0]-=DNz[:,0].mean()
-    DNz[:,1]=(z-zar)**3*0.01
-    DNz[:,1]-=DNz[:,1].mean()
-    T=sacc.Tracer("lsst_sources_%i"%i,"spin2",zar,Nz,exp_sample="lsst_sources",
-                               DNz=DNz)
-    tracers.append(T)
-
-print tracers
-
 # Now, let's have cross-correlation of everything with everything
 # at 100 ell bins for density correlations
 lvals=np.arange(100,1000,100)
-# and at 10 radial bins for cluster weak lensing
-rvals=np.logspace(np.log10(0.5), np.log10(3), 10)
 Ntracer=len(tracers)
-type,bins,t1,q1,t2,q2,val,err,wins=[],[],[],[],[],[],[],[],[]
+type,ell,t1,q1,t2,q2,val,err,wins=[],[],[],[],[],[],[],[],[]
 for t1i in range(Ntracer):
-    ## cluster number counts
-    if tracers[t1i].is_CL():
-        type.append('+N')
-        bins.append(-1.)
-        ## We refer to tracers by their index
-        t1.append(t1i)
-        t2.append(-1)
-        ## Here we have cluster number counts
-        q1.append('S')
-        q2.append('0')
-        ## values and errors
-        val.append(t1i+1)
-        err.append(np.sqrt(float(t1i+1)))
-
     for t2i in range(t1i,Ntracer):
-        ## cluster weak lensing
-        if tracers[t1i].is_CL() and tracers[t2i].is_WL():
-            for r in rvals:
-                ## we have configuration space measurement
-                type.append('+R')
-                ## at this nominal rbins
-                bins.append(r)
-                ## We refer to tracers by their index
-                t1.append(t1i)
-                t2.append(t2i)
-                ## Here we have density-shear cross-correlations
-                q1.append('S')
-                q2.append('E')
-                ## values and errors
-                val.append(np.random.uniform(0,10))
-                err.append(np.random.uniform(1,2))
-            print len(type)
-        if (tracers[t1i].is_CL() or tracers[t2i].is_CL() or tracers[t1i].is_WL() or tracers[t2i].is_WL()) == False:
-            for l in lvals:
-                ## we have Fourier space measurement
-                type.append('FF')
-                ## at this nominal ell
-                bins.append(l)
-                ## but in detail  the measurement
-                ## is a Gaussian window around central ell +/- 50
-                wins.append(sacc.Window(np.arange(l-50,l+50),np.exp(-(1.0*np.arange(-50,50))**2/(2*20.**2))))
-                ## We refer to tracers by their index
-                t1.append(t1i)
-                t2.append(t2i)
-                ## Here we have density cross-correlations so "P" as point
-                ## except for CMB where 
-                q1.append('S')
-                q2.append('S')
-                ## values and errors
-                val.append(np.random.uniform(0,10))
-                err.append(np.random.uniform(1,2))
+        for l in lvals:
+            ## we have Fourier space measurement
+            type.append('FF')
+            ## at this nominal ell
+            ell.append(l)
+            ## but in detail  the measurement
+            ## is a Gaussian window around central ell +/- 50
+            wins.append(sacc.Window(np.arange(l-50,l+50),np.exp(-(1.0*np.arange(-50,50))**2/(2*20.**2))))
+            ## We refer to tracers by their index
+            t1.append(t1i)
+            t2.append(t2i)
+            ## Here we have density cross-correlations so "P" as point
+            ## except for CMB where 
+            q1.append('S')
+            q2.append('S')
+            ## values and errors
+            val.append(np.random.uniform(0,10))
+            err.append(np.random.uniform(1,2))
 
-binning=sacc.Binning(type,bins,t1,q1,t2,q2,windows=wins)
+binning=sacc.Binning(type,ell,t1,q1,t2,q2,windows=wins)
 mean=sacc.MeanVec(val)
 
 
@@ -144,20 +80,11 @@ Np=binning.size()
 cov=np.zeros ((Np,Np))
 for i in range(Np):
     for j in range (i,Np):
-        if binning.binar['type'][i] == '+N':
-            if i == j:
-                cov[i,i] = err[i]*err[i]
-        if binning.binar['type'][i] == '+R':
-            cov[i,j] = err[i]*err[j]
-            if i != j:
-                cov[i,j] /= 10.
-            cov[j,i] = cov[i,j]
-        else:
-            if bins[i]==bins[j]:
-                cov[i,j]=err[i]*err[j]
-                if (i!=j):
-                    cov[i,j]/=10
-                cov[j,i]=cov[i,j]
+        if ell[i]==ell[j]:
+            cov[i,j]=err[i]*err[j]
+            if (i!=j):
+                cov[i,j]/=10
+            cov[j,i]=cov[i,j]
 precision=sacc.Precision(cov,"ell_block_diagonal",is_covariance=True, binning=binning)
             
 ## Add some meta data
