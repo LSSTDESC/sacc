5be8334af58ee86150b9a26b36dd30e07e2959aa
Joe Zuntz <joezuntz@googlemail.com>
2019-03-25 18:55:25 +0000

    add some more data types

--- sacc2/sacc.py
+++ sacc2/sacc.py
@@ -15,8 +15,11 @@ from .utils import unique_list
 allowed_types = [
     "shear_xi_plus",
     "shear_xi_minus",
+    "shear_xi_plus_imaginary",
+    "shear_xi_minus_imaginary",
     "shear_ee",
     "shear_bb",
+    "shear_eb",
     "galaxy_density_cl",
     "galaxy_density_w",
     "ggl_gamma_t",
