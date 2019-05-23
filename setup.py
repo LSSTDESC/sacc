d92ab8dd6dbd6857ac1006b8d53c9b00cfa92959
Joe Zuntz <joezuntz@googlemail.com>
2019-05-23 20:07:05 +0100

    update remaining files

--- setup.py
+++ setup.py
@@ -5,16 +5,9 @@ from distutils.core import setup
 description = "SACC [Save All Correlations and Covariances]"
 
 setup(name="sacc", 
-      version="0.1.0",
+      version="0.2.0",
       description=description,
       url="https://github.com/LSSTDESC/sacc",
       author="LSST DESC",
       packages=['sacc'])
 
-
-setup(name="sacc2", 
-      version="0.1.0",
-      description=description,
-      url="https://github.com/LSSTDESC/sacc",
-      author="LSST DESC",
-      packages=['sacc2'])
--- test/test_sacc2.py
+++ test/test_sacc2.py
@@ -3,7 +3,7 @@ import numpy as np
 import scipy.linalg
 
 def test_construct():
-    s = sacc2.Sacc()
+    s = sacc.Sacc()
 
     # Tracer
     z = np.arange(0., 1.0, 0.01)
@@ -13,25 +13,25 @@ def test_construct():
     for i in range(20):
         ee = 0.1 * i
         tracers = ('source_0', 'source_0')
-        s.add_data_point(sacc2.known_types.galaxy_shear_ee, tracers, ee, ell=10.0*i)
+        s.add_data_point(sacc.known_types.galaxy_shear_ee, tracers, ee, ell=10.0*i)
 
 
 def test_full_cov():
     covmat = np.random.uniform(size=(100,100))
-    C = sacc2.covariance.BaseCovariance.make(covmat, 100)
-    assert isinstance(C, sacc2.covariance.FullCovariance)
+    C = sacc.covariance.BaseCovariance.make(covmat, 100)
+    assert isinstance(C, sacc.covariance.FullCovariance)
     assert np.all(C.covmat==covmat)
     hdu = C.to_hdu()
-    C2 = sacc2.covariance.BaseCovariance.from_hdu(hdu)
+    C2 = sacc.covariance.BaseCovariance.from_hdu(hdu)
     assert np.allclose(C.covmat, C2.covmat)
 
 
 def test_block_cov():
     covmat = [np.random.uniform(size=(50,50)), np.random.uniform(size=(100,100)), np.random.uniform(size=(150,150))]
-    C = sacc2.covariance.BaseCovariance.make(covmat, 300)
-    assert isinstance(C, sacc2.covariance.BlockDiagonalCovariance)
+    C = sacc.covariance.BaseCovariance.make(covmat, 300)
+    assert isinstance(C, sacc.covariance.BlockDiagonalCovariance)
     hdu = C.to_hdu()
-    C2 = sacc2.covariance.BaseCovariance.from_hdu(hdu)
+    C2 = sacc.covariance.BaseCovariance.from_hdu(hdu)
     assert len(C2.blocks)==3
     assert C.block_sizes == C2.block_sizes
     for i in range(3):
@@ -41,13 +41,13 @@ def test_block_cov():
 def test_misc_tracer():
     md1 = {'potato': 'if_necessary', 'answer': 42, 'height':1.83}
     md2 = {'potato': 'never'}
-    T1 = sacc2.BaseTracer.make('misc', 'tracer1', metadata=md1)
-    T2 = sacc2.BaseTracer.make('misc', 'tracer2', metadata=md2)
+    T1 = sacc.BaseTracer.make('misc', 'tracer1', metadata=md1)
+    T2 = sacc.BaseTracer.make('misc', 'tracer2', metadata=md2)
     assert T1.metadata == md1
     assert T2.metadata == md2
 
-    tables = sacc2.BaseTracer.to_tables([T1, T2])
-    D = sacc2.BaseTracer.from_tables(tables)
+    tables = sacc.BaseTracer.to_tables([T1, T2])
+    D = sacc.BaseTracer.from_tables(tables)
 
     T1a = D['tracer1']
     T2a = D['tracer2']
@@ -66,13 +66,13 @@ def test_nz_tracer():
 
     more_nz = {'v1':Nz3, 'v2':Nz4}
 
-    T1 = sacc2.BaseTracer.make('NZ', 'tracer1', z, Nz1, extra_columns=more_nz, metadata=md1)
-    T2 = sacc2.BaseTracer.make('NZ', 'tracer2', z, Nz2, metadata=md2)
+    T1 = sacc.BaseTracer.make('NZ', 'tracer1', z, Nz1, extra_columns=more_nz, metadata=md1)
+    T2 = sacc.BaseTracer.make('NZ', 'tracer2', z, Nz2, metadata=md2)
     assert T1.metadata == md1
     assert T2.metadata == md2
     
-    tables = sacc2.BaseTracer.to_tables([T1, T2])
-    D = sacc2.BaseTracer.from_tables(tables)
+    tables = sacc.BaseTracer.to_tables([T1, T2])
+    D = sacc.BaseTracer.from_tables(tables)
 
     T1a = D['tracer1']
     T2a = D['tracer2']
@@ -89,14 +89,14 @@ def test_mixed_tracers():
     z = np.arange(0., 1., 0.01)
     Nz1 = 1*z # not a sensible N(z)!
     Nz2 = 2*z
-    T1 = sacc2.BaseTracer.make('NZ', 'tracer1', z, Nz1)
-    T2 = sacc2.BaseTracer.make('NZ', 'tracer2', z, Nz2, metadata=md1)
+    T1 = sacc.BaseTracer.make('NZ', 'tracer1', z, Nz1)
+    T2 = sacc.BaseTracer.make('NZ', 'tracer2', z, Nz2, metadata=md1)
 
-    M1 = sacc2.BaseTracer.make("misc", "sample1", metadata=md2)
-    M2 = sacc2.BaseTracer.make("misc", "sample2", metadata=md3)
+    M1 = sacc.BaseTracer.make("misc", "sample1", metadata=md2)
+    M2 = sacc.BaseTracer.make("misc", "sample2", metadata=md3)
 
-    tables = sacc2.BaseTracer.to_tables([T1, M1, T2, M2])
-    recovered = sacc2.BaseTracer.from_tables(tables)
+    tables = sacc.BaseTracer.to_tables([T1, M1, T2, M2])
+    recovered = sacc.BaseTracer.from_tables(tables)
     assert recovered['sample1'].metadata['rank']=='duke'
     assert recovered['sample2'].metadata['robes']==78
     assert np.all(recovered['tracer1'].nz == Nz1)
@@ -106,7 +106,7 @@ def test_inverses():
     N = 25
     C = np.random.uniform(0,1, size=(N,N))
     C = (C+C.T) + np.eye(N)*20
-    M1 = sacc2.BaseCovariance.make(C, N)
+    M1 = sacc.BaseCovariance.make(C, N)
     invC = M1.inverted()
     I = np.dot(invC, C)
     assert np.allclose(I, np.eye(N))
@@ -115,7 +115,7 @@ def test_inverses():
     for b in blocks:
         b += b.T + np.eye(5)*20
 
-    M2 = sacc2.BaseCovariance.make(blocks, N)
+    M2 = sacc.BaseCovariance.make(blocks, N)
     M2dense = np.zeros((N,N))
     for i in range(5):
         M2dense[i*5:i*5+5,i*5:i*5+5] = blocks[i]
@@ -124,7 +124,7 @@ def test_inverses():
     assert np.allclose(I, np.eye(N))
 
     d = abs(np.random.uniform(0,1,size=N))+1
-    M3 = sacc2.BaseCovariance.make(d, N)
+    M3 = sacc.BaseCovariance.make(d, N)
     invC3 = M3.inverted()
     assert np.count_nonzero(invC3 - np.diag(np.diagonal(invC3)))==0
     assert np.allclose(invC3.diagonal() * d, 1)
