4ffee05b109047cf0890f363ea0a0d73f4c79d70
Joe Zuntz <joezuntz@googlemail.com>
2020-09-11 21:09:18 +0100

    version bump to 0.4.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(name="sacc", 
-      version="0.4.3",
+      version="0.4.5",
       description=description,
       long_description=long_description,
       long_description_content_type='text/markdown',
