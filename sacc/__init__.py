37922709b761df44ffb2caec40f474c5c8dd083a
joezuntz <joezuntz@googlemail.com>
2025-10-13 08:45:48 +0100

    Bump version to v2.0.2

--- sacc/__init__.py
+++ sacc/__init__.py
@@ -6,4 +6,4 @@ from .covariance import BaseCovariance  # noqa
 from .tracer_uncertainty import NZLinearUncertainty, NZShiftUncertainty, NZShiftStretchUncertainty  # noqa
 from .io import BaseIO  # noqa
 from . import io  # noqa
-__version__ = '2.0.1' #noqa
+__version__ = '2.0.2' #noqa
